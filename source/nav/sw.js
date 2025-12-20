04d88b1f206177a24d4f8c30ae8a03264c6393c2
worldbuilder957 <worldbuilder957@gmail.com>
2025-12-20 18:20:23 +0800

    增加caticon512

--- source/nav/sw.js
+++ source/nav/sw.js
@@ -1,1 +1,10 @@
 // 这是一个空的 Service Worker
+const ASSETS_TO_CACHE = [
+  '/nav/',
+  '/nav/index.html',
+  '/css/style.css',
+  '/js/script.js',
+  '/nav/manifest.json',
+  '/images/icon-192.png',
+  '/images/icon-512.png',
+];
